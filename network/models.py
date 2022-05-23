31dd87eeeceed6ee5800bcc7733b9898981cfbce
unknown <nestigd95@live.com>
2022-05-24 00:06:42 +0200

    Index.html: made form to add new posts. models.py: started creation of a model for posts. Laid basic CSS structure

--- network/models.py
+++ network/models.py
@@ -2,5 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+    
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    body = models.TextField(blank=True)
+    likes = models.PositiveIntegerField(default=0)
+    created_on = models.DateTimeField(auto_now_add= True)
+    
+
